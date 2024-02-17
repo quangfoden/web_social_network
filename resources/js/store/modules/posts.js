af992cf33e76e35da7ae4f94b05e490849d46e8f
nguyen van quang <nguyenvanquang22103@gmail.com>
2024-02-17 15:36:32 +0700

    fix: feat_post

--- resources/js/store/modules/posts.js
+++ resources/js/store/modules/posts.js
@@ -4,8 +4,11 @@ const state = {
     posts: []
 };
 const mutations = {
-    setPosts(state, posts) {
-        state.posts = posts;
+    allPosts(state, allPosts) {
+        state.posts = allPosts;
+    },
+    setPosts(state, newData) {
+        state.posts.unshift(newData);
     },
 };
 const actions = {
@@ -13,7 +16,7 @@ const actions = {
         axios.get("/api/user/allposts")
             .then(response => {
                 if (Array.isArray(response.data.data.posts)) {
-                    commit('setPosts', response.data.data.posts);
+                    commit('allPosts', response.data.data.posts);
                 } else {
                     console.error('Invalid data format:', response.data.data.posts);
                 }
@@ -22,7 +25,7 @@ const actions = {
                 console.log("Error fetching posts:", error);
             });
     },
-    addNewPost({ commit, dispatch }, formData) {
+    addNewPost({ commit }, formData) {
         axios.post('/api/user/create-post', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
@@ -30,24 +33,22 @@ const actions = {
         })
             .then(response => {
                 if (response.status === 200 && response.data.data.success === true) {
+                    const newData = response.data.data.data
+                    commit('setPosts', newData);
                     Swal.fire({
                         position: "top-end",
                         icon: "success",
                         title: "Bài viết được thêm thành công",
                         showConfirmButton: false,
-                        timer:3000,
+                        timer: 3000,
                     });
-                    setTimeout(() => {
-                        dispatch('fetchPosts');
-                    }, 2000);
                 } else {
-                    // Giải quyết Promise với lỗi
                     Swal.fire({
                         icon: 'error',
                         title: 'Đăng bài viết không thành công',
                         text: `Error`,
                         showConfirmButton: false,
-                        timer:3000
+                        timer: 3000
                     })
                 }
             })
@@ -57,7 +58,7 @@ const actions = {
                     title: ' bài viết không thành công',
                     text: `Error ${error}`,
                     showConfirmButton: false,
-                    timer:3000
+                    timer: 3000
                 })
             });
     },
